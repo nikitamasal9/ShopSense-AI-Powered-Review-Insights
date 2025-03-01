34e4abb0b07640b1842838871055293e6970a9ed
nikitamasal9 <memasalnik9@gmail.com>
2025-03-01 11:18:40 -0500

    Seller: Added the functionalities to add, delete and update products by seller

---
+++ shopsense/products/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('add/', views.add_product, name='add_product'),
+    path('', views.product_list, name='product_list'),
+    path('delete/<int:product_id>/', views.delete_product, name='delete_product'),
+    path('update/<int:product_id>/', views.update_product, name='update_product'),
+
+]
---
+++ shopsense/products/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from .forms import ProductForm
+from .models import Product
+
+def add_product(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('product_list')
+    else:
+        form = ProductForm()
+    return render(request, 'products/add_item.html', {'form': form})
+
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'products/product_list.html', {'products': products})
+
+def delete_product(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    if request.method == 'POST':
+        product.delete()
+        return redirect('product_list')
+    return render(request, 'products/confirm_delete.html', {'product': product})
+
+def update_product(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            return redirect('product_list')
+    else:
+        form = ProductForm(instance=product)
+    return render(request, 'products/update_item.html', {'form': form, 'product': product})
