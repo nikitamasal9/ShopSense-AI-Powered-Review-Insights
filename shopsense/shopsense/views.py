a0a7ec5648a224a826b8ab159b15d9a91e6536f6
najarani <nirajanpoudel77@gmail.com>
2025-03-20 23:27:01 +0545

    Signup and signin page with Customer and Merchant models

--- shopsense/shopsense/views.py
+++ shopsense/shopsense/views.py
@@ -1,16 +1,12 @@
 # shopsense/views.py
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 def home(request):
     return render(request, 'base.html')
 
 def landing_page(request):
+    if not request.user.is_authenticated:
+        return redirect('/auth/signin')
     return render(request, 'landing.html')
 
-
-def sign_in(request):
-    return HttpResponse("Sign in page")
-
-def sign_up(request):
-    return HttpResponse("Sign up page")
