aad598ff4ad432fff96ca1d9312f620410d2aed4
najarani <nirajanpoudel77@gmail.com>
2025-04-05 21:54:09 +0545

    Update the signup forms and navbar menu

--- shopsense/Authentication/views.py
+++ shopsense/Authentication/views.py
@@ -1,22 +1,35 @@
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from django.contrib.auth.models import User
-from django.contrib.auth import login
+from django.contrib.auth import login,logout, authenticate
+from django.contrib import messages
+
 
 from .forms import CustomerSignUpForm, SellerSignUpForm
 
 # Create your views here.
 def user_login(request):
   if request.method == 'POST':
-    print(request.POST)
-    return HttpResponse("Sign in page")
+    email = request.POST.get('email')
+    password = request.POST.get('password')
+    try:
+        user = User.objects.get(email=email)
+        user = authenticate(request, username=user.username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, f"Welcome back, {user.username}!")
+            return redirect('/')
+        else:
+            messages.error(request, "Invalid email or password.")
+    except User.DoesNotExist:
+        messages.error(request, "User does not exist.")
   return render(request, 'signin.html')
-
+  
 def customer_signup(request):
     if request.method == 'POST':
         form = CustomerSignUpForm(request.POST)
         if form.is_valid():
-            user = User.objects.create_user(username=form.cleaned_data['username'], password=form.cleaned_data['password'])
+            user = User.objects.create_user(username=form.cleaned_data['username'],email=form.cleaned_data['email'],password=form.cleaned_data['password'])
             customer = form.save(commit=False)
             customer.user = user
             customer.save()
@@ -31,7 +44,7 @@ def seller_signup(request):
     if request.method == 'POST':
         form = SellerSignUpForm(request.POST)
         if form.is_valid():
-            user = User.objects.create_user(username=form.cleaned_data['username'], password=form.cleaned_data['password'])
+            user = User.objects.create_user(username=form.cleaned_data['username'],email=form.cleaned_data['email'], password=form.cleaned_data['password'])
             seller = form.save(commit=False)
             seller.user = user
             seller.save()
@@ -50,4 +63,12 @@ def onboarding(request):
             return redirect('customer_signup')
         elif user_type == 'seller':
             return redirect('seller_signup')
+        messages.error(request, "Please select a valid user type.")
     return render(request, 'onboarding.html')
+
+def user_logout(request):
+    if request.method == 'POST' or request.method == 'GET':
+        logout(request)
+        messages.success(request, "You have been successfully logged out.")
+        return redirect('home')
+    return render(request, 'logout.html')
